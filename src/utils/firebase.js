86ecf64ad62f0613b66b621a39e0d4135cdbe5e9
MainaMwangiy <charlesmaina8848@gmail.com>
2025-09-13 09:47:17 +0300

    update fields names

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,8 +4,8 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_GITHUB_SSO_API_KEY,
-  authDomain: process.env.REACT_APP_GITHUB_SSO_AUTH_DOMAIN,
+  apiKey: process.env.REACT_APP_FIREBASE_SSO_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_GITHUB_SSO_PROJECT_ID,
   storageBucket: process.env.REACT_APP_GITHUB_SSO_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_GITHUB_SSO_MESSAGING_SENDER_ID,
